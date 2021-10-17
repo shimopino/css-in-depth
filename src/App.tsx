6da4a69c9f69af3e31df8ddd011df99ee01c6c68
KeisukeShimokawa <shimoke4869@gmail.com>
2021-10-17 21:41:44 +0900

    不要なファイルや設定を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import styles from './App.module.scss';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogo} alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className={styles.AppLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className={styles.AppLink}
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>CSS in Depth</h1>
     </div>
   );
 }
